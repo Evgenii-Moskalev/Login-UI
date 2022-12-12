91f4ef5d3c7927c73d128f6d5915400f49ebfd6f
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2022-12-11 20:55:20 -0800

    added index.js and alert functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+let signInBtn = document.getElementById('signInBtn');
+signInBtn.onclick = function () {
+    let inputUsername = document.getElementById('username').value;
+    let inputPassword = document.getElementById('password').value;
+    if (inputUsername != '' & inputPassword != '') {
+        // console.log(inputUsername.length);
+        alert('Your entered Username: ' + inputUsername
+        + ' Your entered Password: ' + inputPassword);
+    }
+}
